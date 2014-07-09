5c9551b6967b1f3f1aa38dc6f13b3ee9c3ba8262
骑兵程序员 <hmjlr123@gmail.com>
2014-07-09 19:41:29 +0800

    回家

--- src/Assets/scripts/BadgerController.cs
+++ src/Assets/scripts/BadgerController.cs
@@ -6,8 +6,13 @@ public class BadgerController : MonoBehaviour {
     public float xDir = 0;
     public float yDir = 1;
 
+    //寻路
+    //private NavMeshAgent monster;
+    //public GameObject target;
 
-    void Awake() {
+    void Start() {
+        //target = GameObject.Find("End");
+        //monster = GetComponent<NavMeshAgent>();
     }
 
     void Update() {
@@ -15,24 +20,20 @@ public class BadgerController : MonoBehaviour {
         //Vector3 target = new Vector3(p.x, p.y - moveSpeed, p.z);
         //transform.position = Vector3.Lerp(transform.position, target, Time.deltaTime);
         rigidbody2D.velocity = new Vector2(0,-1);
+        //monster.SetDestination(target.transform.position);     
     }
 
-    void FixedUpdate() {
-        //if (-1 * rigidbody2D.velocity.y < maxSpeed)
-        //    rigidbody2D.AddForce(Vector2.right * -1 * moveForce);
-        //if (Mathf.Abs(rigidbody2D.velocity.y) > maxSpeed)
-        //    rigidbody2D.velocity = new Vector2(rigidbody2D.velocity.x,Mathf.Sign(rigidbody2D.velocity.y) * maxSpeed);
-
-
-    }
 
     void OnTriggerEnter2D(Collider2D col) {
-        Debug.Log(col.gameObject.tag);
         if (col.gameObject.tag == "Bullet") {
             Destroy(gameObject);
         }
+        else if (col.gameObject.tag == "End") {
+            DestroyObject(GameObject.Find("bunny_1"));
+        }
         else {
-            rigidbody2D.velocity = new Vector2(0, 0);
+            Debug.Log(col.gameObject.tag);
+            rigidbody2D.velocity = new Vector2(0, 1);
             //xDir = 1;
             //yDir = 0;
             //transform.Rotate(0, 0, -90f);
