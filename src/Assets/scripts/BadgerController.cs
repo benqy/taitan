03945029ff0645a3ca6d1a4f44237c16c5532904
骑兵程序员 <hmjlr123@gmail.com>
2014-07-10 19:16:57 +0800

    怪物寻路

--- src/Assets/scripts/BadgerController.cs
+++ src/Assets/scripts/BadgerController.cs
@@ -3,40 +3,60 @@ using System.Collections;
 
 public class BadgerController : MonoBehaviour {
     public float moveSpeed = .1f;
-    public float xDir = 0;
-    public float yDir = 1;
 
-    //寻路
-    //private NavMeshAgent monster;
-    //public GameObject target;
+    public GameObject[] points;
 
+    private int currentIndex = 0;
+    private GameObject currentPoint;
+    private GameObject nextPoint;
     void Start() {
+        for (var i = 0; i <= 11; i++) {
+            points[i] = GameObject.Find("Point" + i);
+        }
         //target = GameObject.Find("End");
         //monster = GetComponent<NavMeshAgent>();
+        
     }
 
     void Update() {
         //var p = transform.position;
         //Vector3 target = new Vector3(p.x, p.y - moveSpeed, p.z);
         //transform.position = Vector3.Lerp(transform.position, target, Time.deltaTime);
-        rigidbody2D.velocity = new Vector2(0,-1);
+        currentPoint = points[currentIndex];
+        nextPoint = points[currentIndex + 1];
+        var direction = nextPoint.transform.position - transform.position;
+        direction.z = 0;
+        direction.Normalize();
+        Vector3 target = direction * moveSpeed + transform.position;
+        transform.position = Vector3.Lerp(transform.position, target, Time.deltaTime);
+        float targetAngle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg - 180;
+
+        //transform.rotation =Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, 0, targetAngle), 5 * Time.deltaTime);
+        transform.rotation = Quaternion.Euler(0, 0, targetAngle);
+        //transform.LookAt(new Vector3(0,0,nextPoint.transform.position.y));
+        
         //monster.SetDestination(target.transform.position);     
     }
 
 
     void OnTriggerEnter2D(Collider2D col) {
-        if (col.gameObject.tag == "Bullet") {
+        Debug.Log(col.name);
+        if (col.tag == "Bullet") {
             Destroy(gameObject);
         }
-        else if (col.gameObject.tag == "End") {
-            DestroyObject(GameObject.Find("bunny_1"));
+        else if (col.tag == "End") {
+           // DestroyObject(GameObject.Find("bunny_1"));
+            Destroy(gameObject);
         }
-        else {
-            Debug.Log(col.gameObject.tag);
-            rigidbody2D.velocity = new Vector2(0, 1);
-            //xDir = 1;
-            //yDir = 0;
-            //transform.Rotate(0, 0, -90f);
+        else if(col.tag == "Point"){
+            var colIndex = System.Convert.ToInt32(col.name.Replace("Point", ""));
+            //
+            //Debug.Log("col" + colIndex);
+            //Debug.Log("curr" + currentIndex);
+            if (colIndex != currentIndex) {
+                currentIndex++;
+                Debug.Log(currentIndex);
+            }
         }
     }
 }
--- src/Assets/scripts/Spawner.cs
+++ src/Assets/scripts/Spawner.cs
@@ -4,22 +4,24 @@ using System.Collections;
 public class Spawner : MonoBehaviour {
 
 
-    public float spawnTime = 2f;
-    public float spawnDelay = 1f;
+    private float spawnTime = 2f;
+    private float spawnDelay = 1f;
     public GameObject[] badgers;
 
     private int badgerIndex = 0;
+    private GameObject begin;
 
     void Start() {
+        begin = (GameObject)GameObject.FindWithTag("Begin");
         InvokeRepeating("Spawn", spawnDelay, spawnTime);
     }
 
 
     void Spawn() {
-        if (badgerIndex < badgers.Length) {
-            GameObject badger = (GameObject)Instantiate(badgers[badgerIndex], new Vector3(4, 3, 0), transform.rotation);
-            badger.transform.Rotate(0, 0, 90f);
-            badgerIndex++;
-        }
+       // if (badgerIndex < badgers.Length) {
+            GameObject badger = (GameObject)Instantiate(badgers[0],begin.transform.position,begin.transform.rotation);
+            //badger.transform.Rotate(0, 0, 90f);
+            //badgerIndex++;
+       // }
     }
 }
